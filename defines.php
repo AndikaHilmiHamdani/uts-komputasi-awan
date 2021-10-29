1077d3655ca03210ed170b1b447f2b463dbc9c73
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2021-10-29 12:35:50 +0700

    first commit

---
+++ defines.php
@@ -0,0 +1,5 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '1246408792435576' );
+	define( 'INSTAGRAM_APP_SECRET', '12bf44332c30b94e8726e01c195b0bed' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'http://localhost/sosial_media/' );
+?>
